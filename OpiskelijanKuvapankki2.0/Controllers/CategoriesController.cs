1bbd510dabeac96fa2c0eb69756d2b858019c67b
Olli Koski <olli.koski@student.careeria.fi>
2025-03-26 17:58:56 +0200

    Kategorian poistotoiminto

--- OpiskelijanKuvapankki2.0/Controllers/CategoriesController.cs
+++ OpiskelijanKuvapankki2.0/Controllers/CategoriesController.cs
@@ -19,6 +19,59 @@ namespace OpiskelijanKuvapankki2_0.Controllers
             return Ok(categories);
         }
 
+        [HttpPost]
+        public ActionResult AddNewCategory([FromBody] Category newcategory)
+        {
+            
+                try
+                {
+                if(db.Categories.Count() > 15) {
+                    return BadRequest("Kategorioita ei voi lisätä enempää!");//rajoitetaan kategorioiden määrää
+                }
+                    db.Categories.Add(newcategory);
+                    db.SaveChanges();
+                    return Ok($"Lisättiin uusi kategoria {newcategory.CategoryName}");
+                }
+                catch (Exception e)
+                {
+                    return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+                }
+            
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult RemoveUnUsedCategory(int id)
+        {
+            try
+            {
+
+                var categ = db.Categories.Find(id);
+
+                if (categ == null)
+                {
+
+                    return NotFound("Kategoriaa" + " ei löytynyt.");
+                }
+
+                var catState = db.Images.Where(k => k.CategoryId == categ.CategoryId).FirstOrDefault();
+                
+                if(catState != null)//Estää poistamisen jos kategoria on käytössä
+                {
+                    return BadRequest("Kategoria on käytössä!");
+                }
+
+                db.Categories.Remove(categ);
+                db.SaveChanges();
+                return Ok("Poistettiin kategoria" + categ.CategoryName);
+                
+                
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+
 
     }
 
