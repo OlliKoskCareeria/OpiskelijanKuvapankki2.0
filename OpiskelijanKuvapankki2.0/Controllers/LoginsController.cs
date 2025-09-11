b22a7b46444d39ef15f7320cd23eed567be650d4
Olli Koski <olli.koski@student.careeria.fi>
2025-09-11 13:35:08 +0300

    Ilogger otettu käyttöön

--- OpiskelijanKuvapankki2.0/Controllers/LoginsController.cs
+++ OpiskelijanKuvapankki2.0/Controllers/LoginsController.cs
@@ -1,11 +1,45 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OpiskelijanKuvapankki2_0.Models;
+using OpiskelijanKuvapankki2_0.Services;
+using Microsoft.Extensions.Logging;
+
 
 namespace OpiskelijanKuvapankki2_0.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class LoginsController : ControllerBase
+    public class LoginsController(OpiskelijanKuvapankki2_0Context _db, UserService _userservice, ILogger<LoginsController> _logger) : ControllerBase
     {
+        private readonly OpiskelijanKuvapankki2_0Context db = _db;
+        private readonly UserService userservice = _userservice;
+        private readonly ILogger<LoginsController> logger = _logger;
+
+
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Login newuser)
+        {
+            try
+            {
+                var existingLogin = db.Logins.FirstOrDefault(l => l.Email == newuser.Email);//tarkistetaan onko käyttäjätunnus käytössä
+                if (existingLogin != null)
+                {
+                    logger.LogInformation("käyttäjä varattu");
+                    return BadRequest(new { Message = "Käyttäjätunnus on varattu" });
+                }
+                if (newuser.Email.EndsWith(userservice.BasicUser()) || newuser.Email == userservice.AdminUser())//tarkistetaan että sähköposti on hyväksytty
+                {
+                    db.Logins.Add(newuser);
+                    db.SaveChanges();
+                    
+                    return Ok($"Lisättiin uusi Kayttaja {newuser.Email}");
+                }
+                else { return BadRequest(new { Message = "Tunnusta ei voi luoda tälle sähköpostiosoitteelle" }); }
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
     }
 }
--- OpiskelijanKuvapankki2.0/Program.cs
+++ OpiskelijanKuvapankki2.0/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using OpiskelijanKuvapankki2_0.Models;
 using OpiskelijanKuvapankki2_0.Services;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -18,7 +22,13 @@ builder.Services.AddDbContext<OpiskelijanKuvapankki2_0Context>(options => option
 
 builder.Services.AddScoped<ImageService>();
 
+builder.Services.AddScoped<UserService>();
+
+
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
